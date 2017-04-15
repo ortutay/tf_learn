0d1caeb1fd7373bbd1a87bdc621f34207455febb
ortutay <marcell.ortutay@gmail.com>
2017-04-15 10:58:22 -0700

    custom model

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def three():
     print 'W: %s, b: %s, loss: %s' % (curr_W, curr_b, curr_loss)
 
 
-def four():
+def contrib_learn():
     features = [tf.contrib.layers.real_valued_column('x', dimension=1)]
     estimator = tf.contrib.learn.LinearRegressor(feature_columns=features)
     x = np.array([1.,2.,3.,4.])
@@ -72,5 +72,34 @@ def four():
     print estimator.evaluate(input_fn=input_fn)
 
 
+def model(features, labels, mode):
+    W = tf.get_variable('W', [1], dtype=tf.float64)
+    b = tf.get_variable('b', [1], dtype=tf.float64)
+    y = W * features['x'] + b
+
+    loss = tf.reduce_sum(tf.square(y - labels))
+
+    global_step = tf.train.get_global_step()
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    train = tf.group(optimizer.minimize(loss),
+                     tf.assign_add(global_step, 1))
+
+    return tf.contrib.learn.ModelFnOps(
+        mode=mode,
+        predictions=y,
+        loss=loss,
+        train_op=train)
+
+def run_custom_model():
+    estimator = tf.contrib.learn.Estimator(model_fn=model)
+    x = np.array([1., 2., 3., 4.])
+    y = np.array([0., -1., -2., -3.])
+    input_fn = tf.contrib.learn.io.numpy_input_fn(
+        {'x': x}, y, 4, num_epochs=1000)
+
+    estimator.fit(input_fn=input_fn, steps=1000)
+    print estimator.evaluate(input_fn=input_fn, steps=10)
+
+
 if __name__ == '__main__':
-    four()
+    run_custom_model()
